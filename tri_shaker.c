f32095edae62cad64b623926e583d16bf0d07cbc
Matancy <24440469+Matancy@users.noreply.github.com>
2021-12-07 19:29:03 +0100

    Fonction inversion + afficher

--- tri_shaker.c
+++ tri_shaker.c
@@ -7,3 +7,20 @@ void tri_shaker(tableau);
 void inversion(tableau, int, int);
 void afficher(tableau);
 
+// Inversion de deux valeurs
+void inversion(tableau tab, int val1, int val2)
+{
+    int tmp;
+    tmp = tab[val2];
+    tab[val2] = tab[val1];
+    tab[val1] = tmp;
+}
+
+// Affichage du tableau
+void afficher(tableau tab)
+{
+    for (int i = 0; i < N; i++)
+    {
+        printf("%d ", tab[i]);
+    }
+}
