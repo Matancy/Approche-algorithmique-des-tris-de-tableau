322bb19d57878fb83a69229f5040f2e2f1da6e61
Matancy <24440469+Matancy@users.noreply.github.com>
2021-12-07 19:30:00 +0100

    Début tri shaker
    + Système d'inversion de l'ordre
    + Variables de fonctionnement
    + Boucle de fonctionnement tant que les deux bouts ne se sont pas rencontrés

--- tri_shaker.c
+++ tri_shaker.c
@@ -17,6 +17,43 @@ int main()
     afficher(t);
     return EXIT_SUCCESS;
 }
+
+void tri_shaker(tableau tab)
+{
+    int indMin, indMax, count, order;
+    indMin = 0;     // Indice du min trié dans le tableau
+    indMax = N - 1; // Indice du max trié dans le tableau
+    count = 0;      // Compteur
+    order = 0;      // 0 => Evolution croissante, 1 => Evolution décroissante
+
+    while ((indMax - indMin) != 0) // Tant que l'écart entre le minimum et le max est différent de 0 (soit tableau trié)
+    {
+        // Partie croissante
+        if (order == 0)
+        {
+            if (tab[count] > tab[count + 1])
+            {
+                // Si la première valeur est plus grande, on inverse les deux
+                inversion(tab, count, count + 1);
+            }
+        }
+
+        // Inversion en fonction de l'avancée du tri
+        if (count == (N - 1) || count == 1)
+        {
+            // Inversion du mode d'évolution
+            if (order == 0)
+            {
+                order = 1; // Passage en évolution décroissante
+            }
+            else
+            {
+                order = 0; // Passage en évolution croissante
+            }
+        }
+    }
+}
+
 // Inversion de deux valeurs
 void inversion(tableau tab, int val1, int val2)
 {
