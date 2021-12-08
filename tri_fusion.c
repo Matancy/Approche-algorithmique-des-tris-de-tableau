d478efab3734f22d20c1416dc514c32b81636804
lostanlen_jeremie <jeremie.lostanlen@etudiant.univ-rennes1.fr>
2021-12-08 19:52:55 +0100

    Poursuite de l'implémentation du tri fusion

--- tri_fusion.c
+++ tri_fusion.c
@@ -1,33 +1,33 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include<stdbool.h>
+#include <stdbool.h>
 
 #define MAX 10
 typedef int typeTab[MAX];
 
 int main()
 {
-    typeTab tablo = {3,6,54,23,56,18,99,0,67,78};
+    typeTab tablo = {3, 6, 54, 23, 56, 18, 99, 0, 67, 78};
 }
-
 
 //procedure de tri fusion:
 void tri_fusion(typeTab tablo)
 {
     typeTab autreTablo;
-    int i, j; //compteurs pour les deux tableaux
-    int deb; //Indice du début d'un tableau
-    int fin; //Indice de fin d'un tableau
-    int millieu; //Indice du milleiu d'un tableau
-    bool trié; //Vérifie si la première partie partie du tableau est triée
+    int i, j;        //compteurs pour les deux tableaux
+    int deb;         //Indice du début d'un tableau
+    int fin;         //Indice de fin d'un tableau
+    int millieu;     //Indice du milleiu d'un tableau
+    bool estTrie;    //Vérifie si la première partie partie du tableau est triée
+    int nbrElements; //Nombre d'éléments d'un tableau
     deb = 0;
-    fin = MAX-1;
-    while (deb < fin)
+    fin = MAX - 1;
+    while (!estTrie)
     {
-        
-        millieu = (deb + fin)/2;
-        //pour la partie gauche du tableau:
-        fin = millieu;
+        if (deb < fin)
+        {
+            
+        }
     }
-    
 }
+
