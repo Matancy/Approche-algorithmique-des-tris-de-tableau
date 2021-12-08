892d187f84da82b71c75a9d799fb4cac99824b06
Mathéo Tichy <matheo.tichy@gmail.com>
2021-12-08 20:47:18 +0100

    Tri insertion fini
    * Modification pour utiliser un seul tri
    + Ajout demande croissant / décroissant
    + Ajout différents tableaux favorables, non favorable, normal
    + Ajout fonction affichage

--- tri_insertion.c
+++ tri_insertion.c
@@ -2,44 +2,50 @@
 #include <stdio.h>
 #define MAX 10
 
-void tri_insertion_permutation(typeTab t);
-void tri_insertion_decalage(typeTab t);
+typedef int tableau[MAX];
 
-typedef int typeTab[10];
+void tri_insertion(tableau, int);
+void afficher(tableau);
 
 int main()
 {
-    int choixOrdre; // Sert à définir l'ordre dans lequel l'utilisateur veut trier son tableau
-    int choixTri;   // Sert à définir quel tri l'utilisateur souhaite utiliser
-    typeTab t = {54, 50, 21, 12, 76, 23, 78, 1, 0, 56};
-    printf("Dans quel ordre voulez-vous trier votre tableau:\n");
-    printf("\t1 - Tri par insertion avec permutations successives\n");
-    printf("\t2 - Tri par insertion avec décalage à droite puis insertion\n\n");
+    tableau favorable = {1, 3, 5, 6, 8, 9, 12, 34, 56, 78};
+    tableau nonfavorable = {78, 56, 34, 12, 9, 8, 6, 5, 3, 1};
+    tableau normal = {12, 45, 2, 4, 23, 90, 65, 45, 99, 19};
+
+    int choixOrdre; // Croissant : 0, Décroissant : 1
+
+    printf("Entrez l'ordre de tri :\n");
+    printf("\t0 - Croissant\n");
+    printf("\t1 - Decroissant\n\n");
     scanf("%d", &choixOrdre);
-    switch (choixOrdre)
-    {
-    case 1:
 
-        break;
+    printf("Tableau favorable :\n");
+    afficher(favorable);
+    tri_insertion(favorable, choixOrdre);
+    printf("Tableau trie :\n");
+    afficher(favorable);
+    printf("\n\n");
 
-    default:
-        break;
-    }
-    printf("Indiquez le tri que vous souhaitez utiliser pour trier votre tableau :\n");
-    printf("\t1 - Tri par insertion avec permutations successives\n");
-    printf("\t2 - Tri par insertion avec décalage à droite puis insertion\n");
-    scanf("%d", &choixTri);
-    if (choixTri == 1)
-    {
-        tri_insertion_permutation(t);
-    }
-    else if (choixTri == 2)
-    {
-        tri_insertion_decalage(t);
-    }
+    printf("Tableau non favorable :\n");
+    afficher(nonfavorable);
+    tri_insertion(nonfavorable, choixOrdre);
+    printf("Tableau trie :\n");
+    afficher(nonfavorable);
+    printf("\n\n");
+
+    printf("Tableau normal :\n");
+    afficher(normal);
+    tri_insertion(normal, choixOrdre);
+    printf("Tableau trie :\n");
+    afficher(normal);
+    printf("\n\n");
+
+    return EXIT_SUCCESS;
 }
 
-void tri_insertion_permutation(typeTab t)
+// Tri par insertion des tableaux
+void tri_insertion(tableau t, int ordre)
 {
     int i;
     int j;
@@ -47,42 +53,37 @@ void tri_insertion_permutation(typeTab t)
     for (i = 1; i < MAX; i++)
     {
         j = i;
-        if (t[i] < t[i - 1])
+        // Ordre croissant
+        if ((ordre == 0) && (t[i] < t[i - 1]))
         {
-            while (t[j] < t[j - 1])
+            while (t[j] < t[j - 1] && (j != 0))
             {
                 temp = t[j - 1];
                 t[j - 1] = t[j];
                 t[j] = temp;
                 j = j - 1;
             }
-            j = i;
+        }
+        // Ordre décroissant
+        else if ((ordre == 1) && (t[i - 1] < t[i]))
+        {
+            while (t[j - 1] < t[j] && (j != 0))
+            {
+                temp = t[j];
+                t[j] = t[j - 1];
+                t[j - 1] = temp;
+                j = j - 1;
+            }
         }
     }
-    for (i = 0; i < MAX; i++)
-    {
-        printf("%d", t[i]);
-    }
-    return EXIT_SUCCESS;
 }
 
-void tri_insertion_decalage(typeTab t)
+// Affichage d'un tableau
+void afficher(tableau tab)
 {
-    int conserve; //conserve la valeur t[i]
-    int i;
-    int j;
-    for (i = 0; i < MAX; i++) //on parcours le tableau à trier
+    for (int i = 0; i < MAX; i++)
     {
-        j = i;
-        conserve = t[i];
-        do
-        {
-            if (conserve < t[j])
-            {
-                t[i] = t[j]; //on met la valeur t[j] dans l'espace libéré
-            }
-            j--;
-        } while ((t[j] > conserve) && (j >= 0));
-        t[j] = conserve;
+        printf("%d ", tab[i]);
     }
+    printf("\n");
 }
