b0ded20fa29332ca8f39712286d8154ad3dd400d
Mathéo Tichy <matheo.tichy@gmail.com>
2021-12-15 20:43:01 +0100

    Tri rapide fonctionnel de taille n
    * Correction d'un bug sur le tri (tri qui enlevait un élément)
    + Implémentation de la taille N
    + Implémentation croissant / décroiissant

--- tri_rapide.c
+++ tri_rapide.c
@@ -1,46 +1,59 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
-#define MAX 10
+#define TAB_MAX 10
 
-typedef int tableau[MAX];
+typedef int tableau[TAB_MAX];
 
-void tri_rapide(tableau, int, int);
+void tri_rapide(tableau, int, int, int);
 void permutation(tableau, int, int);
 void afficher(tableau);
+void creationTableauAlea(tableau, int);
 
 int main()
 {
-    tableau favorable = {1, 3, 5, 6, 8, 9, 12, 34, 56, 78};
-    tableau nonfavorable = {78, 56, 34, 12, 9, 8, 6, 5, 3, 1};
-    tableau normal = {12, 45, 2, 4, 23, 90, 65, 45, 99, 19};
+    // tableau normal = {12, 45, 2, 4, 23, 90, 65, 45, 99, 19};
+    // tableau favorable = {1, 3, 5, 6, 8, 9, 12, 34, 56, 78};
+    // tableau nonfavorable = {78, 56, 34, 12, 9, 8, 6, 5, 3, 1};
+    tableau tab;
 
-    printf("Tableau normal : \n");
-    afficher(normal);
-    printf("Tableau trie : \n");
-    tri_rapide(normal, 0, MAX);
-    afficher(normal);
-    printf("\n\n");
+    int ordre; // Croissant : 0, Décroissant : 1
 
-    printf("Tableau favorable :\n");
-    afficher(favorable);
-    tri_rapide(favorable, 0, MAX);
-    printf("Tableau trie :\n");
-    afficher(favorable);
-    printf("\n\n");
+    printf("Entrez l'ordre de tri :\n");
+    printf("\t0 - Croissant\n");
+    printf("\t1 - Decroissant\n\n");
+    scanf("%d", &ordre);
 
-    printf("Tableau non favorable :\n");
-    afficher(nonfavorable);
-    tri_rapide(nonfavorable, 0, MAX);
-    printf("Tableau trie :\n");
-    afficher(nonfavorable);
-    printf("\n\n");
+    creationTableauAlea(tab, TAB_MAX);
+    tri_rapide(tab, 0, TAB_MAX, ordre);
+    afficher(tab);
+
+    // printf("Tableau normal : \n");
+    // afficher(normal);
+    // printf("Tableau trie : \n");
+    // tri_rapide(normal, 0, TAB_MAX - 1, ordre);
+    // afficher(normal);
+    // printf("\n\n");
+
+    // printf("Tableau favorable :\n");
+    // afficher(favorable);
+    // tri_rapide(favorable, 0, TAB_MAX - 1, ordre);
+    // printf("Tableau trie :\n");
+    // afficher(favorable);
+    // printf("\n\n");
+
+    // printf("Tableau non favorable :\n");
+    // afficher(nonfavorable);
+    // tri_rapide(nonfavorable, 0, TAB_MAX - 1, ordre);
+    // printf("Tableau trie :\n");
+    // afficher(nonfavorable);
+    // printf("\n\n");
 
     return EXIT_SUCCESS;
 }
 
 // Tri rapide
-void tri_rapide(tableau tab, int debut, int fin)
+void tri_rapide(tableau tab, int debut, int fin, int ordre)
 {
     int pivot, i, j;
     if (debut < fin)
@@ -50,21 +63,45 @@ void tri_rapide(tableau tab, int debut, int fin)
         j = fin;
         while (i < j)
         {
-            // Approche jusqu'au pivot
-            while (tab[i] <= tab[pivot] && i < fin)
-                i++;
+            // Ordre croissant
+            if (ordre == 0)
+            {
+                // Approche jusqu'au pivot
+                while (tab[i] <= tab[pivot] && i < fin)
+                {
+                    i++;
+                }
 
-            // Approche juste avant le pivot
-            while (tab[j] > tab[pivot])
-                j--;
+                // Approche juste avant le pivot
+                while (tab[j] > tab[pivot])
+                {
+                    j--;
+                }
+            }
+            // Ordre décroissant
+            else
+            {
+                // Approche jusqu'au pivot
+                while (tab[i] >= tab[pivot] && i < fin)
+                {
+                    i++;
+                }
+
+                // Approche juste avant le pivot
+                while (tab[j] < tab[pivot])
+                {
+                    j--;
+                }
+            }
             if (i < j)
             {
                 permutation(tab, i, j);
             }
         }
+
         permutation(tab, pivot, j);
-        tri_rapide(tab, debut, j - 1);
-        tri_rapide(tab, j + 1, fin);
+        tri_rapide(tab, debut, j - 1, ordre);
+        tri_rapide(tab, j + 1, fin, ordre);
     }
 }
 
@@ -80,9 +117,23 @@ void permutation(tableau tab, int a, int b)
 // Affichage d'un tableau
 void afficher(tableau tab)
 {
-    for (int i = 0; i < MAX; i++)
+    for (int i = 0; i < TAB_MAX; i++)
     {
         printf("%d ", tab[i]);
     }
     printf("\n");
+}
+
+// Fonction de remplissage aléatoire
+void creationTableauAlea(tableau tab, int taille)
+{
+    for (int i = 0; i < taille; i++)
+    {
+        int nb;
+        do
+        {
+            nb = rand() % RAND_MAX;
+        } while (nb == 0);
+        tab[i] = nb;
+    }
 }
