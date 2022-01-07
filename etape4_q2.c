06a88a026d5469293de76f8f03211074044069ad
Matancy <matheo.tichy@gmail.com>
2022-01-07 15:13:01 +0100

    Nettoyage fichier

--- etape4_q2.c
+++ etape4_q2.c
@@ -2,9 +2,6 @@
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
-#include "tri_shaker.c"
-#include "tri_rapide.c"
-#include "tri_insertion.c"
 
 // Taille maximale des tableaux d'entiers et de caracteres.
 #define TAB_MAX 500000
@@ -24,6 +21,10 @@ void convertToChar(tableau, tableauchar);
 void afficherString(tableaustring);
 void concatenateChar(tableauchar, tableaustring);
 void copierTableau(tableaustring t, tableaustring u);
+
+void tri_shaker(tableaustring, int, unsigned long *, unsigned long *);
+void tri_rapide(tableaustring, int, int, int, unsigned long *, unsigned long *);  
+void tri_insertion(tableaustring, int, unsigned long *, unsigned long *);
 
 int main()
 {
@@ -77,6 +78,8 @@ int main()
     /**************************************
                 TRI SHAKER
      **************************************/
+    nbComparaison = 0;
+    nbPermutations = 0;
     printf("\n\n\tTRI SHAKER\n\n");
      begin = clock(); // Enregistrement de l'heure de début
     tri_shaker(tabstring2, ordre, &nbComparaison, &nbPermutations);
@@ -90,6 +93,8 @@ int main()
     /**************************************
                 TRI RAPIDE    
      **************************************/
+    nbComparaison = 0;
+    nbPermutations = 0;
     printf("\n\n\tTRI RAPIDE\n\n");
     begin = clock(); // Enregistrement de l'heure de début
     tri_rapide(tabstring3, 0, ordre, TAB_STRING, &nbComparaison, &nbPermutations);
@@ -175,6 +180,9 @@ void concatenateChar(tableauchar tabchar, tableaustring tabstring)
         count = count + 1;
     }
 }
+//
+// Fonctions communes aux trois tris
+//
 
 // Fonction pour remplir les cases vides d'un tableau par des espaces
 void init(tableaustring tabstring)
@@ -184,7 +192,6 @@ void init(tableaustring tabstring)
         strcpy(tabstring[i], "          ");
     }
 }
-
 // Copier un tableau dans un autre tableau
 void copierTableau(tableaustring t, tableaustring u)
 {
@@ -194,7 +201,6 @@ void copierTableau(tableaustring t, tableaustring u)
         strcpy(u[i], t[i]); 
     }
 }
-
 // Inversion de deux valeurs
 void inversion(tableaustring tab, int val1, int val2)
 {
@@ -203,7 +209,6 @@ void inversion(tableaustring tab, int val1, int val2)
     strcpy(tab[val2], tab[val1]);
     strcpy(tab[val1], tmp);
 }
-
 // Permutation de deux éléments
 void permutation(tableaustring tab, int a, int b)
 {
@@ -211,4 +216,214 @@ void permutation(tableaustring tab, int a, int b)
     strcpy(tmp, tab[a]);
     strcpy(tab[a], tab[b]);
     strcpy(tab[b], tmp);
+}
+
+//
+// Tris
+// 
+void tri_shaker(tableaustring tab, int ordre_des_donnees, unsigned long *nbrCompare, unsigned long *nbrInverse)
+{
+    int indMin, indMax, count, order;
+    indMin = 0;              // Indice du min trié dans le tableau
+    indMax = TAB_STRING - 1; // Indice du max trié dans le tableau
+    count = 0;               // Compteur
+    order = 0;               // 0 => Evolution croissante, 1 => Evolution décroissante
+    int comparaison;         // Négatif quand le premier élément du strcmp est supérieur au second, positif dans le cas inverse et nul sinon
+    while ((indMax - indMin) != 0) // Tant que l'écart entre le minimum et le max est différent de 0 (soit tableau trié)
+    {
+        // Partie croissante
+        if (order == 0)
+        {
+            // Tri croissant
+            if (ordre_des_donnees == 0)
+            {
+                comparaison = strcmp(tab[count], tab[count + 1]);
+                *nbrCompare = *nbrCompare + 1;
+                if (comparaison > 0)
+                {
+                    // Si la première valeur est plus grande, on inverse les deux
+                    inversion(tab, count, count + 1);
+                    *nbrInverse = *nbrInverse + 1;
+                }
+            }
+            // Tri décroissant
+            else if (ordre_des_donnees == 1)
+            {
+                comparaison = strcmp(tab[count], tab[count + 1]);
+                *nbrCompare = *nbrCompare + 1;
+                if (comparaison < 0)
+                {
+                    // Si la première valeur est plus petite, on inverse les deux
+                    inversion(tab, count + 1, count);
+                    *nbrInverse = *nbrInverse + 1;
+                }
+            }
+            // Incrémentation du compteur
+            count = count + 1;
+        }
+        // Partie décroissante
+        else
+        {
+            // Tri croissant
+            if (ordre_des_donnees == 0)
+            {
+                comparaison = strcmp(tab[count - 1], tab[count]);
+                *nbrCompare = *nbrCompare + 1;
+                if (comparaison > 0)
+                {
+                    // Si la valeur en dessous est plus grande, on inverse les deux
+                    inversion(tab, count - 1, count);
+                    *nbrInverse = *nbrInverse + 1;
+                }
+            }
+            // Tri décroissant
+            else if (ordre_des_donnees == 1)
+            {
+                comparaison = strcmp(tab[count - 1], tab[count]);
+                *nbrCompare = *nbrCompare + 1;
+                if (comparaison < 0)
+                {
+                    // Si la valeur en dessous est plus petite, on inverse les deux
+                    inversion(tab, count, count - 1);
+                    nbrInverse = nbrInverse + 1;
+                }
+            }
+            // Décrémentation du compteur
+            count = count - 1;
+        }
+        // Inversion en fonction de l'avancée du tri
+        if ((count == indMax) || (count == indMin))
+        {
+            // Inversion du mode d'évolution
+            if (order == 0)
+            {
+                order = 1;           // Passage en évolution décroissante
+                indMax = indMax - 1; // Décrémentation de l'indice maximum
+                count = count - 1;
+            }
+            else
+            {
+                order = 0;           // Passage en évolution croissante
+                indMin = indMin + 1; // Incrémentation de l'indice minimum
+                count = count + 1;
+            }
+        }
+    }
+}
+void tri_rapide(tableaustring tab, int debut, int fin, int ordre, unsigned long *nbrCompare, unsigned long *nbrInverse)
+{
+    int comparaison; // Négatif quand le premier élément du strcmp est supérieur au second, positif dans le cas inverse et nul sinon
+    int pivot, i, j;
+    if (debut < fin)
+    {
+        pivot = debut;
+        i = debut;
+        j = fin;
+        while (i < j)
+        {
+            // Ordre croissant
+            if (ordre == 0)
+            {
+                // Approche jusqu'au pivot
+                comparaison = strcmp(tab[i], tab[pivot]);
+                *nbrCompare = *nbrCompare + 1;
+                while (((comparaison < 0) || (comparaison == 0)) && (i < fin))
+                {
+                    i++;
+                    comparaison = strcmp(tab[i], tab[pivot]);
+                }
+                // Approche juste avant le pivot
+                comparaison = strcmp(tab[j], tab[pivot]);
+                *nbrCompare = *nbrCompare + 1;
+                while (comparaison > 0)
+                {
+                    j--;
+                    comparaison = strcmp(tab[j], tab[pivot]);
+                }
+            }
+            // Ordre décroissant
+            else
+            {
+                // Approche jusqu'au pivot
+                comparaison = strcmp(tab[i], tab[pivot]);
+                *nbrCompare = *nbrCompare + 1;
+                while (((comparaison > 0) || (comparaison == 0)) && (i < fin))
+                {
+                    i++;
+                    comparaison = strcmp(tab[i], tab[pivot]);
+                }
+
+                // Approche juste avant le pivot
+                comparaison = strcmp(tab[j], tab[pivot]);
+                *nbrCompare = *nbrCompare + 1;
+                while (comparaison < 0)
+                {
+                    j--;
+                    comparaison = strcmp(tab[j], tab[pivot]);
+                }
+            }
+            if (i < j)
+            {
+                permutation(tab, i, j);
+                *nbrInverse = *nbrInverse + 1;
+            }
+        }
+        permutation(tab, pivot, j);
+        *nbrInverse = *nbrInverse + 1;
+        tri_rapide(tab, debut, j - 1, ordre, nbrCompare, nbrInverse);
+        tri_rapide(tab, j + 1, fin, ordre, nbrCompare, nbrInverse);
+    }
+}
+void tri_insertion(tableaustring t, int ordre, unsigned long *nbComparaison, unsigned long *nbPermutation)
+{
+    int comparaison; // Négatif quand le premier élément du strcmp est supérieur au second, positif dans le cas inverse et nul sinon
+    int i;
+    int j;
+    char temp[11]; // Variable temporaire pour faire un échange
+    for (i = 1; i < TAB_STRING; i++)
+    {
+        j = i;
+        // Ordre croissant
+        if (ordre == 0)
+        {
+            comparaison = strcmp(t[i], t[i - 1]);
+            *nbComparaison = *nbComparaison + 1;
+            if (comparaison < 0) // Si t[i] est inférieur à t[i - 1]
+            {
+                while ((comparaison < 0) && (j > 0))
+                {
+                    strcpy(temp, t[j - 1]);
+                    strcpy(t[j - 1], t[j]);
+                    strcpy(t[j], temp);
+                    *nbPermutation = *nbPermutation + 1;
+                    j--;
+                    comparaison = strcmp(t[j], t[j - 1]);
+                    *nbComparaison = *nbComparaison + 1;
+                }
+            }
+        }
+        // Ordre décroissant
+        else if (ordre == 1)
+        {
+            comparaison = strcmp(t[i - 1], t[i]);
+            *nbComparaison = *nbComparaison + 1;
+            if (comparaison < 0) // Si t[i - 1] est inférieur à t[i]
+            {
+                while ((comparaison < 0) && (j > 0))
+                {
+                    strcpy(temp, t[j]);
+                    strcpy(t[j], t[j - 1]);
+                    strcpy(t[j - 1], temp);
+                    *nbPermutation = *nbPermutation + 1;
+                    j--;
+                    comparaison = strcmp(t[j - 1], t[j]);
+                    *nbComparaison = *nbComparaison + 1;
+                }
+            }
+        }
+        else
+        {
+            *nbComparaison = *nbComparaison + 1;
+        }
+    }
 }
